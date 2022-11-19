272c7529cab2b04d4b1d18837ff0e75dbdeb03e2
Brian Terry <brianter@amazon.com>
2022-11-18 20:07:33 -0800

    add step2

---
+++ walkthrough/testing_before/step2/app/app.py
@@ -0,0 +1,49 @@
+import re
+from aws_cdk.core import (
+    App,
+    Environment,
+    Aspects,
+)
+from cdk_nag import ( AwsSolutionsChecks, NagSuppressions, NagPackSuppression )
+import os
+import pathlib
+from s3_deployment import S3AppStack
+
+app = App()
+
+stack = S3AppStack(app, "policy-as-code",
+           env=Environment(
+               account=os.environ["CDK_DEFAULT_ACCOUNT"],
+               region=os.environ["CDK_DEFAULT_REGION"]
+           ),
+           description='')
+
+#Uncomment to enable Aspects
+Aspects.of(app).add(AwsSolutionsChecks())
+
+#NagSuppressions.add_resource_suppressions_by_path(stack, "/policy-as-code/AutomationAssumeRole/Resource", suppressions=[
+#    NagPackSuppression(
+#        id='AwsSolutions-IAM4',
+#        reason='No need to rotate',
+#    )
+#])
+#NagSuppressions.add_resource_suppressions_by_path(stack, "/policy-as-code/Bucket/Key/Resource", suppressions=[
+#    NagPackSuppression(
+#        id='AwsSolutions-KMS5',
+#        reason='No needed for this demo',
+#    )
+#])
+
+#NagSuppressions.add_resource_suppressions_by_path(stack, "/policy-as-code/AutomationAssumeRole/Resource", suppressions=[
+#    NagPackSuppression(
+#        id='AwsSolutions-IAM5',
+#        reason='No needed for this demo',
+#    )
+#])
+#NagSuppressions.add_resource_suppressions_by_path(stack, "/policy-as-code/Bucket/Resource", suppressions=[
+#    NagPackSuppression(
+#        id='AwsSolutions-S1',
+#        reason='No access logs required for this bucket',
+#    )
+#])
+app.synth()
